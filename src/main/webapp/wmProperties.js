4872ccdcb0e12b728749c534253f42ede1825ac6
KavyaSree <kavya.sree@wavemaker.com>
2017-11-08 13:43:49 +0530

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,12 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "cool-blue",
+  "defaultLanguage" : "en",
+  "displayName" : "sample_june29_6pm",
+  "homePage" : "Main",
+  "name" : "sample_june29_6pm",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
